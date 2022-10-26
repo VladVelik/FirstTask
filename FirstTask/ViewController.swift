37f3cb87f1677cb0dacf54b52865d991694b8ade
VladVelik <sosinvladik@yandex.ru>
2022-10-26 20:46:10 +0300

    Initial Commit

---
+++ FirstTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstTask
+//
+//  Created by Sosin Vladislav on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
